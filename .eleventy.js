86370e9efaeebd609ba5cbcb349edb5abd29d4d2
Gary Kaganas <kaganasg@gmail.com>
2021-06-14 08:48:25 -0400

    Updates eleventy config to find style file

--- .eleventy.js
+++ .eleventy.js
@@ -65,7 +65,7 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.setUseGitIgnore(false);
   eleventyConfig.addWatchTarget("./_tmp/style.css");
-  eleventyConfig.addPassthroughCopy({ "_tmp/style.css": "style.css" });
+  eleventyConfig.addPassthroughCopy({ "./_tmp/style.css": "style.css" });
 
   eleventyConfig.addShortcode("version", function () {
     return String(Date.now());
